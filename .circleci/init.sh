a95652ec94cc74c41633cd0bd97598e4a0dc31d0
Khusein Bespalyi <khusein.bespalyi@otolane.com>
2018-03-27 14:45:03 -0400

    #0022: added Makefile to .circleci

--- .circleci/init.sh
+++ .circleci/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This shell script runs when the docker image starts. 
+# This shell script runs when the docker image starts.
 # In this script, you configure your cluster parameters.
 echo "starting ...."
 wait_for_start() {
@@ -17,17 +17,17 @@ if [ -n "${COUCHBASE_NAME:+1}" ]; then
 
     echo "add node to  cluster"
     # wait for couchbase clustering to be setup
-    wait_for_start couchbase-cli server-list -c couchbase:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD
+    wait_for_start couchbase-cli server-list -c couchbase:8091 --username $ADMIN_LOGIN --password $ADMIN_PASSWORD
 
     echo "launch couchbase"
     /entrypoint.sh couchbase-server &
 
     # wait for couchbase to be up (this is the local couchbase belonging to this container)
-    wait_for_start couchbase-cli server-info -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD
+    wait_for_start couchbase-cli server-info -c localhost:8091 --username $ADMIN_LOGIN --password $ADMIN_PASSWORD
 
     # add this new node to the cluster
     ip=`hostname --ip-address`
-    #couchbase-cli server-add -c couchbase -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --server-add=$ip:8091 --server-add-username=$ADMIN_LOGIN --server-add-password=$ADMIN_PASSWORD
+    #couchbase-cli server-add -c couchbase --cluster-username $ADMIN_LOGIN --cluster-password $ADMIN_PASSWORD --server-add=$ip:8091 --server-add-username=$ADMIN_LOGIN --server-add-password=$ADMIN_PASSWORD
 
     echo "node added to cluster"
 
@@ -37,21 +37,21 @@ if [ -n "${COUCHBASE_NAME:+1}" ]; then
     echo "adding and rebalancing ..."
 
     # rebalance
-    couchbase-cli rebalance -c couchbase -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --server-add=$ip:8091 --server-add-username=$ADMIN_LOGIN --server-add-password=$ADMIN_PASSWORD --services=data,index,query
+    couchbase-cli rebalance -c couchbase --username $ADMIN_LOGIN --password $ADMIN_PASSWORD --server-add=$ip:8091 --server-add-username=$ADMIN_LOGIN --server-add-password=$ADMIN_PASSWORD --services=data,index,query
 else
 
     echo "launch couchbase"
     /entrypoint.sh couchbase-server &
 
     # wait for couchbase to be up
-    wait_for_start couchbase-cli server-info -c localhost:8091 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD
+    wait_for_start couchbase-cli server-info -c localhost:8091 --username $ADMIN_LOGIN --password $ADMIN_PASSWORD
 
     echo "start initial cluster configuration"
     # init the cluster
-    couchbase-cli cluster-init -c 127.0.0.1 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --cluster-init-username=${ADMIN_LOGIN} --cluster-init-password=${ADMIN_PASSWORD} --cluster-init-port=8091 --cluster-init-ramsize=512 --services=data,index,query
+    couchbase-cli cluster-init -c 127.0.0.1 --cluster-username $ADMIN_LOGIN --cluster-password $ADMIN_PASSWORD --cluster-port=8091 --cluster-index-ramsize=256 --services=data,index,query --cluster-ramsize 512
 
     # create bucket data
-    couchbase-cli bucket-create -c 127.0.0.1 -u $ADMIN_LOGIN -p $ADMIN_PASSWORD --bucket=default --bucket-type=couchbase --bucket-ramsize=256 --wait
+    couchbase-cli bucket-create -c 127.0.0.1 --username $ADMIN_LOGIN --password $ADMIN_PASSWORD --bucket=default --bucket-type=couchbase --bucket-ramsize=256 --wait
 fi
 
 wait
